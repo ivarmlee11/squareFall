93fab8e718b075d0842caa808a2fe85e7e44b81c
Ivar Lee <ivarr5@gmail.com>
2016-06-09 07:29:21 -0700

    commented a bit

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
   // establish a firebase object to store and read data from
+
   var myFirebaseRef = new Firebase('https://squarefall.firebaseio.com/');
   document.addEventListener('DOMContentLoaded', function() {
     var playerObject;
@@ -13,6 +14,10 @@
     var playerName = prompt('Enter your name!');
     var stopped = false;
 
+
+    // my game area object which includes some properties with values
+    // that are functions to start and end the game
+
     var myGameArea = {
       canvas: document.createElement('canvas'),
       startTheGame: function() {
@@ -23,14 +28,20 @@
         this.ticker = 0;
         updateBoardInterval = setInterval(updateGameArea, 20);
         window.addEventListener('keydown', function (e) {
+
           // a property of .keys is checked to see if it exists or if it should be set to
           // an empty array
+
           myGameArea.keys = (myGameArea.keys || []);
+
           // the keydown event is passed as a value to the keys property
+
           myGameArea.keys[e.keyCode] = true;
         });
         window.addEventListener('keyup', function (e) {
+
           // when the key is released it is removed from the myGameArea
+
           myGameArea.keys[e.keyCode] = false;
         });
       },
@@ -64,23 +75,26 @@
       });
     };
 
-
+    // stop game function, the 'stopped' flag is set to true to allow the
+    // following functions to run in the if statement
 
     var stopGame = function() {
       clearInterval(updateBoardInterval);
       if(!stopped) {
-      stopped = true;
-      scoreBox.innerHTML = 'You got ' + score + ' points!';
-      button.className = ('');
-      firebaseInteraction();
-      leaderBoardDisplay();
-
-
-    }
+        stopped = true;
+        scoreBox.innerHTML = 'You got ' + score + ' points!';
+        button.className = ('');
+        firebaseInteraction();
+        leaderBoardDisplay();
+      }
     };
 
     // constructor function; this function is a template for all my objects
+
     var PieceConstructor = function(width, height, color, x, y) {
+
+      // sets these params to properties of the object
+
       this.width = width;
       this.height = height;
       this.x = x;
@@ -88,15 +102,24 @@
       this.sideSpeed = 0;
       this.verticalSpeed = 0;
 
+      // these properties contain functions that describe how
+      // these objects will be drawn on the canvas
+
       this.update = function() {
         var ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
       };
+
+      // this function describes the speed and direction
+
       this.position = function() {
         this.x += this.sideSpeed;
         this.y += this.verticalSpeed;
       };
+
+      // this function covers hit detection
+
       this.collideCheck = function(otherobj) {
         var left = this.x;
         var right = this.x + (this.width);
@@ -107,10 +130,16 @@
         var othertop = otherobj.y;
         var otherbottom = otherobj.y + (otherobj.height);
         var hitDetect = true;
+
+        // this if statement detects hits vs other objects
+
         if ((bottom < othertop) || (top > otherbottom) || (right < otherleft) ||
          (left > otherright)) {
           hitDetect = false;
         }
+
+        // this if statement detects hits vs walls
+
         if ((bottom > myGameArea.canvas.height + 30) || (top < -100) ||
         (right > myGameArea.canvas.width) || (left < 0)) {
           stopGame();
@@ -118,6 +147,9 @@
         return hitDetect;
       };
     };
+
+    // start game function that makes a player object and sets up the stopped
+    // flag for later use in our stopgame function
 
     var startGame = function() {
       stopped = false;
@@ -152,7 +184,12 @@
         }
       }
 
+      // this clears the 'old' objects
+
       myGameArea.clear();
+
+
+      // display message until an arrow is pressed
 
       if (!myGameArea.keys) {
         scoreBox.innerHTML = 'Use the arrow keys to dodge the platforms';
@@ -169,25 +206,35 @@
         playerObject.verticalSpeed = 0.5;
         playerObject.y = playerObject.y + 2.5;
       }
-      myGameArea.ticker += 1;
 
+      // game ticker iterates by one every time the game area refreshes
+      // the game area refreshes based on the param passed to timeforobstacle--
+
+      myGameArea.ticker += 1;
       if (myGameArea.ticker === 1 || timerForObstacleRelease(60)) {
         score += 20;
+        console.log(score + ' score ' + "ticker");
         scoreBox.innerHTML = score;
 
-      // allows for random obstacle length
+        // allows for random obstacle length
 
         minWidth = 20;
         maxWidth = 200;
         width = Math.floor(Math.random() * (maxWidth - minWidth) + minWidth);
+
         // allows for random gap between obstacles
+
         minGap = 100;
         maxGap = 200;
         gap = Math.floor(Math.random() * (maxGap - minGap + 1));
+
         // obstacles being pushed to an array
+
         platforms.push(new PieceConstructor((width - gap), 4, 'red', 260, 330));
         platforms.push(new PieceConstructor(width, 4, 'red', 0, 430));
+
         // obstacle that appears every 100 points
+
         if (score % 100 === 0) {
           platforms.push(new PieceConstructor((width / 4), 10, 'red', (width - gap), 530));
         }
@@ -196,6 +243,10 @@
       // for loop that iterates through array of objects and brings them to the top of the
 
       for (var i = 0; i < platforms.length; i += 1) {
+
+        // they are brought towards the top of the canvas because their
+        // 'y' property is decremented by 2 pixes every time the game area is refreshed
+
         platforms[i].y += -2;
         platforms[i].update();
       }
@@ -203,8 +254,10 @@
       playerObject.position();
       playerObject.update();
     };
+
     startGame();
     button.addEventListener('click', function() {
       window.location.reload();
+
     });
   });
